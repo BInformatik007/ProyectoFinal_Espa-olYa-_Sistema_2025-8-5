eb84914a648a14c1ac29dc230dc3debb73ad5537
Claudio <binformatik007@gmail.com>
2025-04-20 13:40:41 -0400

    Hice cambios en las lecciones.

---
+++ LP_Esp/js/lessons-test.js
@@ -0,0 +1,26 @@
+import { supabase } from './supabaseClient.js';
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const lessonId = urlParams.get('id');
+
+    if (!lessonId) {
+        console.error("No se especificó ningún ID de lección.");
+        return;
+    }
+
+    const { data, error } = await supabase
+        .from('lessons')
+        .select('title, description')
+        .eq('id', lessonId)
+        .single();
+
+    if (error) {
+        console.error('Error al obtener la lección:', error.message);
+        return;
+    }
+
+    // Asignamos el título y la descripción al HTML
+    document.querySelector('.main-lessons-tests__preview-title').textContent = data.title;
+    document.querySelector('.main-lessons-tests__preview-text').textContent = data.description;
+});
